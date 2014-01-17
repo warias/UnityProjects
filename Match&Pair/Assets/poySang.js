daf4eb6c829d076c5c7a93d39bda00b3888a9bba
rosevared <rucussadoria@programmer.in.th>
2014-01-17 17:25:29 +0700

    Test Flare

---
+++ Match&Pair/Assets/poySang.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var pic22 : LensFlare;
+var leftmouse : KeyCode;
+
+
+function Start () {
+ pic22.brightness = 0;
+
+}
+
+function Update () {
+	if(Input.GetKey(leftmouse) == true){
+		if(pic22.brightness == 1){
+			pic22.brightness = 0;
+		}else{
+			pic22.brightness = 1;
+		}
+	
+	}
+
+}
